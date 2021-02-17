d305fd3fb3f0a7e618230e88b87e68caf360b52f
David Toledo <dtoledoz0207@gmail.com>
2021-02-17 16:42:48 -0600

    initial-commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Custom Hooks
+
+## useCounter
+Para crear un contador y usarlo en un componente.
+
+## useFetch
+Para hacer peticiones a un endpoint de una API y mostrar la siguiente informacion en el componente que se esta usando (data, loading, error).
+
+## useForm
+Para manejar los datos de los inputs en formularios que usamos en un componente.
---
+++ hooks/useFetch.js
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from 'react';
+
+
+
+export const useFetch = (url) => {
+
+  const isMounted = useRef(true);
+  const [state, setState] = useState({data: null, loading: true, error: null});
+
+
+  useEffect(() => {
+
+    return () => {
+      isMounted.current = false;
+    }
+  }, []);
+
+  useEffect(() => {
+
+    setState({data: null, loading: true, error: null});
+
+    fetch(url).then(response => response.json()).then(data => {
+
+      if (isMounted.current) {
+        setState({
+          loading: false,
+          error: null,
+          data: data
+        });
+      }
+
+    }).catch(() => {
+
+      setState({
+        loading: false,
+        error: 'No se pudo cargar la info',
+        data: null
+      });
+      
+    });
+  }, [url]);
+
+
+  return state;
+}
